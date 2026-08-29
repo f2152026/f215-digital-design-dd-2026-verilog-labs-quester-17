0e51043f8c39a56358cd423af3a1a7e6f4d49cf2
SNEHIL RAJ <f20250536@pilani.bits-pilani.ac.in>
2026-08-29 05:59:40 +0000

    lab01 task5

--- labs/lab01/task5/CLA64_hier.v
+++ labs/lab01/task5/CLA64_hier.v
@@ -36,4 +36,225 @@ module cla64_hier(
 
   // TODO: your hierarchical design goes here.
 
+ 
+  wire [15:0] Pblk;
+  wire [15:0] Gblk;
+
+ 
+  wire [16:0] c;
+
+  
+  wire [15:0] block_cout;
+
+  assign #(2) c[0] = cin;
+
+
+
+
+  genvar i;
+  generate
+    for (i = 0; i < 16; i = i + 1) begin : gen_cla_blocks
+
+      cla4 block (
+        .a(a[i*4 +: 4]),
+        .b(b[i*4 +: 4]),
+        .cin(c[i]),
+        .sum(sum[i*4 +: 4]),
+        .cout(block_cout[i]),
+        .Pblk(Pblk[i]),
+        .Gblk(Gblk[i])
+      );
+
+    end
+  endgenerate
+
+
+  
+
+  assign #(2) c[1] = Gblk[0] |
+                     (Pblk[0] & cin);
+
+
+  assign #(2) c[2] = Gblk[1] |
+                     (Pblk[1] & Gblk[0]) |
+                     (Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[3] = Gblk[2] |
+                     (Pblk[2] & Gblk[1]) |
+                     (Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[4] = Gblk[3] |
+                     (Pblk[3] & Gblk[2]) |
+                     (Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[5] = Gblk[4] |
+                     (Pblk[4] & Gblk[3]) |
+                     (Pblk[4] & Pblk[3] & Gblk[2]) |
+                     (Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[6] = Gblk[5] |
+                     (Pblk[5] & Gblk[4]) |
+                     (Pblk[5] & Pblk[4] & Gblk[3]) |
+                     (Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                     (Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[7] = Gblk[6] |
+                     (Pblk[6] & Gblk[5]) |
+                     (Pblk[6] & Pblk[5] & Gblk[4]) |
+                     (Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                     (Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                     (Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[8] = Gblk[7] |
+                     (Pblk[7] & Gblk[6]) |
+                     (Pblk[7] & Pblk[6] & Gblk[5]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[9] = Gblk[8] |
+                     (Pblk[8] & Gblk[7]) |
+                     (Pblk[8] & Pblk[7] & Gblk[6]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                     (Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[10] = Gblk[9] |
+                      (Pblk[9] & Gblk[8]) |
+                      (Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[11] = Gblk[10] |
+                      (Pblk[10] & Gblk[9]) |
+                      (Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[12] = Gblk[11] |
+                      (Pblk[11] & Gblk[10]) |
+                      (Pblk[11] & Pblk[10] & Gblk[9]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[13] = Gblk[12] |
+                      (Pblk[12] & Gblk[11]) |
+                      (Pblk[12] & Pblk[11] & Gblk[10]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Gblk[9]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[14] = Gblk[13] |
+                      (Pblk[13] & Gblk[12]) |
+                      (Pblk[13] & Pblk[12] & Gblk[11]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Gblk[10]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Gblk[9]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[15] = Gblk[14] |
+                      (Pblk[14] & Gblk[13]) |
+                      (Pblk[14] & Pblk[13] & Gblk[12]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Gblk[11]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Gblk[10]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Gblk[9]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) c[16] = Gblk[15] |
+                      (Pblk[15] & Gblk[14]) |
+                      (Pblk[15] & Pblk[14] & Gblk[13]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Gblk[12]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Gblk[11]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Gblk[10]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Gblk[9]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Gblk[8]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Gblk[7]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Gblk[6]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Gblk[5]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Gblk[4]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Gblk[3]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Gblk[2]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Gblk[1]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Gblk[0]) |
+                      (Pblk[15] & Pblk[14] & Pblk[13] & Pblk[12] & Pblk[11] & Pblk[10] & Pblk[9] & Pblk[8] & Pblk[7] & Pblk[6] & Pblk[5] & Pblk[4] & Pblk[3] & Pblk[2] & Pblk[1] & Pblk[0] & cin);
+
+
+  assign #(2) cout = c[16];
+
 endmodule
--- labs/lab01/task5/cla4.v
+++ labs/lab01/task5/cla4.v
@@ -20,20 +20,78 @@
 //
 // TODO -- Step 3: sum bits
 //   sum[i] = p[i] ^ c[i]     (c0 = cin)
-
 module cla4(
   input  [3:0] a,
   input  [3:0] b,
   input        cin,
   output [3:0] sum,
-  output       cout
+  output       cout,
+  output       Pblk,
+  output       Gblk
 );
 
   wire p0, p1, p2, p3;
   wire g0, g1, g2, g3;
   wire c1, c2, c3;
-
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
+
+  
+  xor #(2) (p0, a[0], b[0]);
+  xor #(2) (p1, a[1], b[1]);
+  xor #(2) (p2, a[2], b[2]);
+  xor #(2) (p3, a[3], b[3]);
+
+  
+  and #(2) (g0, a[0], b[0]);
+  and #(2) (g1, a[1], b[1]);
+  and #(2) (g2, a[2], b[2]);
+  and #(2) (g3, a[3], b[3]);
+
+  
+  wire t20;
+  wire t30, t31;
+  wire t40, t41, t42;
+  wire t50, t51, t52, t53;
+
+  
+  and #(2) (t20, p0, cin);
+  or  #(2) (c1, g0, t20);
+
+
+  and #(2) (t30, p1, g0);
+  and #(2) (t31, p1, p0, cin);
+  or  #(2) (c2, g1, t30, t31);
+
+  
+  and #(2) (t40, p2, g1);
+  and #(2) (t41, p2, p1, g0);
+  and #(2) (t42, p2, p1, p0, cin);
+  or  #(2) (c3, g2, t40, t41, t42);
+
+  
+  and #(2) (t50, p3, g2);
+  and #(2) (t51, p3, p2, g1);
+  and #(2) (t52, p3, p2, p1, g0);
+  and #(2) (t53, p3, p2, p1, p0, cin);
+  or  #(2) (cout, g3, t50, t51, t52, t53);
+
+  
+  xor #(2) (sum[0], p0, cin);
+  xor #(2) (sum[1], p1, c1);
+  xor #(2) (sum[2], p2, c2);
+  xor #(2) (sum[3], p3, c3);
+
+  
+  assign #(2) Pblk = p0 & p1 & p2 & p3;
+
+
+  assign #(2) Gblk = g3 |
+                     (p3 & g2) |
+                     (p3 & p2 & g1) |
+                     (p3 & p2 & p1 & g0);
+
+
+
 
 endmodule
