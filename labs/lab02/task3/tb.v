1952f781bd05b08c44a2fed5bb3febcbdfd5d02c
SNEHIL RAJ <f20250536@pilani.bits-pilani.ac.in>
2026-09-19 06:06:52 +0000

    lab02 task3

---
+++ labs/lab02/task3/tb.v
@@ -0,0 +1,43 @@
+// tb.v task 3
+
+module tb;
+
+  reg  [1:0] t_a;
+  reg  [1:0] t_b;
+  wire       t_gt;
+  wire       t_lt;
+  wire       t_eq;
+
+  integer i, j;
+
+  comp2 DUT (
+    .A(t_a),
+    .B(t_b),
+    .GT(t_gt),
+    .LT(t_lt),
+    .EQ(t_eq)
+  );
+
+  string vcd_file;
+  initial begin
+    if ($value$plusargs("vcd=%s", vcd_file)) begin
+      $dumpfile(vcd_file);
+      $dumpvars(0, tb);
+    end
+  end
+
+  initial begin
+    for (i = 0; i < 4; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        t_a = i;
+        t_b = j;
+        #5;
+      end
+    end
+    $finish;
+  end
+
+  initial
+    $monitor($time, " A=%b B=%b | GT=%b LT=%b EQ=%b", t_a, t_b, t_gt, t_lt, t_eq);
+
+endmodule
